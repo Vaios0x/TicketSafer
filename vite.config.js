6f85d85407729ba8a6c809fd417b8cfd4e97a6a7
Vaios0x <vaiogioss@gmail.com>
2025-07-09 14:04:04 -0600

    1-01

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: process.cwd(),
+  base: './',
   server: {
     port: 3005,
     hmr: {
@@ -12,6 +15,10 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: fileURLToPath(new URL('./index.html', import.meta.url))
+    }
   }
 }) 
